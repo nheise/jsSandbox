f35d5a7027a577c393fa5d059ee42f99f50b809f
nheise <n(dot)heise(at)heise(minus)media(dot)de>
2012-05-11 22:06:04 +0200

    improve aspect after

--- aspects/aspects.js
+++ aspects/aspects.js
@@ -6,22 +6,38 @@ Aspects.after = function( method, object, aspect ) {
     var original = object[ method ];
     
     object[ method ] = function() {
-        var args = original.apply( this, arguments );
-        return ret = aspect.apply( this, Array( args, null ) );
+        var originalReturnValue = original.apply( this, arguments );
+        
+        pushValueTo( originalReturnValue, arguments );
+        
+        console.log(arguments);
+        
+        aspect.apply( this, arguments );
+        
+        return originalReturnValue;
     };
+    
+    function pushValueTo( originalReturnValue, arguments ) {
+       arguments[ nextArgumentNumber( arguments ) ] = originalReturnValue;
+    }
+    
+    function nextArgumentNumber( arguments ) {
+       return Object.keys(arguments).length;
+    }
 };
 
 function A() {
     
-    this.methodA = function( arg ) {
-        console.log( 'method A ' + arg );
+    this.methodA = function( arg1, arg2 ) {
+        console.log( 'method A ' + arg1 + " # " + arg2 );
+        return "return value from A";
     };
 }
 
 var a = new A();
 
-Aspects.after( 'methodA', a, function() {
-  console.log( "after methodA" );  
+Aspects.after( 'methodA', a, function( arg1, arg2, returnValue ) {
+  console.log( "after call methodA with " + arg1 + " # " + arg2 + " # " + returnValue );  
 } );
 
-a.methodA( 'arg1' );
+console.log( a.methodA( 'arg1', 'arg2' ) ); 
